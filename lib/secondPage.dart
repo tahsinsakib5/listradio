d0374bfff422a7f7b9b57182999e7c7c134f025a
Abir Hossain <abir2526@gmail.com>
2023-09-28 23:34:53 +0600

    Commit Message

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -5,33 +5,31 @@ import 'package:listradio/aquiz.dart';
 import 'package:slide_countdown/slide_countdown.dart';
 
 class HomePage extends StatefulWidget {
-   HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-   Color? bgcolor =Colors.white;
-    int radiog=0;
-
+  int radiog = 0;
+  Color? bgcolor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
+      body: Column(
         children: [
-           SizedBox(
+          SizedBox(
             height: 20,
-           ),
+          ),
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: SlideCountdown(
-              padding:EdgeInsets.all(20),
-            duration: const Duration(minutes:10),
+              padding: EdgeInsets.all(20),
+              duration: const Duration(minutes: 10),
+            ),
           ),
-          ),
-
           Expanded(
             child: Container(
               color: Colors.amber,
@@ -39,42 +37,45 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.all(20),
                 child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: allQuiz.length,itemBuilder: (context, index) =>
-                
-                 Padding(
-                   padding: const EdgeInsets.all(10),
-                   child: Card(
-                    
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        
-                        Padding(
-                          padding: const EdgeInsets.all(35),
-                          child: Text(allQuiz[index].question,style: TextStyle(fontSize: 27),),
+                  itemCount: allQuiz.length,
+                  itemBuilder: (context, quizIndex) {
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Card(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(35),
+                              child: Text(
+                                allQuiz[quizIndex].question,
+                                style: TextStyle(fontSize: 27),
+                              ),
+                            ),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: 4,
+                              itemBuilder: (context, answerIndex) {
+                                return RadioListTile(
+                                    selected: true,
+                                    title: Text(allQuiz[answerIndex]
+                                        .answerList[answerIndex]
+                                        .answer),
+                                    value: answerIndex,
+                                    groupValue: radiog,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        radiog = value!;
+                                      });
+                                    });
+                              },
+                            )
+                          ],
                         ),
-                      
-                        ListView.builder(
-                          shrinkWrap: true,
-                          itemCount:4,itemBuilder: (context, index) =>
-                          RadioListTile(
-                          title: Text(allQuiz[index].answerList[index].answer),
-                          value:index, 
-                          groupValue: radiog,
-                          
-                           onChanged:(value){
-                           
-                            
-                        }),
-                         
-                        
-                        )
-                           
-                        
-                      ],
-                    ),
-                           ),
-                 ),),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ),
